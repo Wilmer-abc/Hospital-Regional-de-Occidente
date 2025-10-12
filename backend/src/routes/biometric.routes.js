1fd39b7a9a9726fa227b180f429652a431e86995
Wbatz <admidael23@gmail.com>
2025-10-12 11:30:46 -0600

    correcion en rutas

--- backend/src/routes/biometric.routes.js
+++ backend/src/routes/biometric.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
-const svc = require('../services/biometric/hikvision.service');
+const svc = require('../services/biometric/hikvision.service.js');
 
 // Validacion defensiva: falla en arranque si falta algo
 ['requireAuth', 'requireAdmin'].forEach((k) => {
