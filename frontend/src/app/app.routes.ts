26d75b82e9b2f05fdc83715b68cb93f062128cc5
Wbatz <admidael23@gmail.com>
2025-10-12 11:38:24 -0600

    se agregaron rutas de reportes y notificaciones

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { AccesoDenegadoComponent } from './components/acceso-denegado/acceso-den
 import { AuthGuard } from './guards/auth.guard';
 import { EmpleadosComponent } from './components/empleados/empleados.component';
 import { AsignarTurnosComponent } from './components/asignar-turnos/asignar-turnos.component';
+import { NotificacionesComponent } from './components/notificaciones/notificaciones.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -29,6 +31,21 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     data: { roles: ['rrhh', 'jefe'] }
   },
+
+  {
+    path: 'notificaciones',             
+    component: NotificacionesComponent,  
+    canActivate: [AuthGuard],
+    data: { roles: ['rrhh', 'jefe'] }
+  },
+
+  {
+    path: 'reportes',             
+    component: ReportesComponent,  
+    canActivate: [AuthGuard],
+    data: { roles: ['rrhh', 'jefe'] }
+  },
+
 
   { path: '**', redirectTo: 'dashboard' }
 ];
