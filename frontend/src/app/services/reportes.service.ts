403e7283ebc19fab71c38f86409965331ed57b11
Wbatz <admidael23@gmail.com>
2025-10-12 11:52:34 -0600

    creacio de archivo de reportes

---
+++ frontend/src/app/services/reportes.service.ts
@@ -0,0 +1,23 @@
+// frontend/src/app/services/reportes.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+const API = environment.apiBase + '/reportes';
+
+@Injectable({ providedIn: 'root' })
+export class ReportesService {
+  constructor(private http: HttpClient) {}
+
+  getAreas() {
+    return this.http.get<any>(`${API}/areas`);
+  }
+
+  getReporte(areaId: number, desde: string, hasta: string) {
+    const params = new HttpParams()
+      .set('area_id', areaId)
+      .set('desde', desde)
+      .set('hasta', hasta);
+    return this.http.get<any>(`${API}/asistencia`, { params });
+  }
+}
