064c8584e057463f8adf2a05b746ad8db71936e2
Wbatz <admidael23@gmail.com>
2025-10-27 16:04:29 -0600

    cambios dentro de nuestro archivo

--- frontend/src/app/services/reportes.service.ts
+++ frontend/src/app/services/reportes.service.ts
@@ -1,4 +1,3 @@
-// frontend/src/app/services/reportes.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -7,22 +6,23 @@ const API = environment.apiBase + '/reportes';
 
 @Injectable({ providedIn: 'root' })
 export class ReportesService {
-  obtenerAsistencia(areaSeleccionada: number, desde: any, hasta: any) {
-    throw new Error('Method not implemented.');
-  }
   constructor(private http: HttpClient) {}
 
   getAreas() {
     return this.http.get<any>(`${API}/areas`);
   }
 
-  getReporte(areaId: number, desde: string, hasta: string) {
-    const params = new HttpParams()
-      .set('area_id', areaId)
-      .set('desde', desde)
-      .set('hasta', hasta);
+  getReporte(areaId: number, desde: string, hasta: string, tipoReporte: string = 'semana') {
+    let params = new HttpParams()
+      .set('area_id', areaId.toString())
+      .set('tipo_reporte', tipoReporte);
+
+    if (desde && hasta) {
+      params = params
+        .set('desde', desde)
+        .set('hasta', hasta);
+    }
+
     return this.http.get<any>(`${API}/asistencia`, { params });
   }
-
-  
 }
